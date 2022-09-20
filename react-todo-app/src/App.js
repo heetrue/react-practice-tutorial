4f3a8149ac5130f17fb5073c27a64cbfad532539
heetrue <bonnylovesyou@naver.com>
2022-09-20 23:05:00 +0900

    :sparkles: feat: Add todo list data

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 export default class App extends Component {
+  btnStyle = {
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right',
+  };
+
+  getStyle = () => {
+    return {
+      padding: '10px',
+      borderBottom: '1px dotted #ccc',
+      textDecoration: 'none',
+    };
+  };
+
+  todoData = [
+    {
+      id: '1',
+      title: '공부하기',
+      completed: true,
+    },
+    {
+      id: '2',
+      title: '청소하기',
+      completed: false,
+    },
+  ];
+
   render() {
     return (
       <div className='container'>
@@ -8,6 +38,13 @@ export default class App extends Component {
           <div className='title'>
             <h1>할 일 목록</h1>
           </div>
+          {this.todoData.map((data) => (
+            <div style={this.getStyle()} key={data.id}>
+              <input type='checkbox' defaultChecked={data.completed} />
+              {data.title}
+              <button style={this.btnStyle}>X</button>
+            </div>
+          ))}
         </div>
       </div>
     );
